93407c54748ae16a20ab9095c3f833abe153e3c3
Kadapalla Nithin Kumar <kadapallanithin@gmail.com>
2025-06-13 23:55:39 +0530

    Added MountainCar-v0 and it's analysis

---
+++ RL/q_learning/MountainCar-v0/README.md
@@ -0,0 +1,102 @@
+# Mountain Car
+Training for 5000 episodes gave a good model.<br>
+But despite increasing number of episodes from 5000 to 20000, produced a bad model.
+
+Trained with <br>
+g : 0.9<br>
+lr : 0.9<br>
+## With np seed 0
+### During training
+|(5k) Failure %|(5k) Avg reward|(20k) Failure %|(20k)Avg reward|
+|---|---|---|---|
+|63.72|-181.2134|63.715|-187.05025|
+
+For 5000 episodes
+
+![](MountainCar-v0_5000_np_0_d_0.0004_g_0.9_lr_0.9.jpg "Mountain Car")
+
+For 20000 episodes
+
+![](MountainCar-v0_20000_np_0_d_0.0001_g_0.9_lr_0.9.jpg "Mountain Car")
+
+#### During testing
+|Seed|(5k) Failure %|(5k) Avg reward|(20k) Failure %|(20k)Avg reward|
+|---|---|---|---|---|
+|0|0.0| -147.865|10.0|-168.907|
+|1|0.0| -148.537|6.6 |-168.040|
+|2|0.0| -148.597|6.5 |-167.974|
+|3|0.0| -147.695|7.2 |-167.804|
+|4|0.0| -148.495|7.8 |-168.508|
+|5|0.0| -148.393|8.5 |-168.598|
+|6|0.0| -148.275|8.3 |-168.513|
+|7|0.0| -148.839|6.2 |-167.895|
+|8|0.0| -148.296|7.2 |-168.007|
+|9|0.0| -148.347|7.9 |-168.463|
+
+For 5000 episodes
+
+![](MountainCar-v0_testing_5000.jpg "Mountain Car")
+
+For 20000 episodes
+
+![](MountainCar-v0_testing_20000.jpg "Mountain Car")
+
+
+## With np seed 42
+#### During training
+|(5k) Failure %|(5k) Avg reward|(20k) Failure %|(20k)Avg reward|
+|---|---|---|---|
+|71.06|-191.9536|65.285|-184.94605|
+
+For 5000 episodes
+
+![](MountainCar-v0_5000_np_42_d_0.0004_g_0.9_lr_0.9.jpg "Mountain Car")
+
+For 20000 episodes
+
+![](MountainCar-v0_20000_np_42_d_0.0001_g_0.9_lr_0.9.jpg "Mountain Car")
+
+#### During testing
+|Seed|(5k) Failure %|(5k) Avg reward|(20k) Failure %|(20k)Avg reward|
+|---|---|---|---|---|
+|0|26.9| -188.272|35.9| -172.224|
+|1|26.3| -187.883|31.6| -169.344|
+|2|23.4| -187.730|32.5| -169.032|
+|3|29.0| -188.712|35.5| -172.080|
+|4|25.7| -187.437|32.0| -169.347|
+|5|24.4| -187.865|36.1| -170.791|
+|6|25.2| -188.523|34.8| -170.930|
+|7|25.4| -187.128|30.6| -168.272|
+|8|24.4| -187.451|34.3| -170.648|
+|9|24.4| -187.670|33.2| -170.322|
+
+For 5000 episodes
+
+![](MountainCar-v0_testing_5000_42.jpg "Mountain Car")
+
+For 20000 episodes
+
+![](MountainCar-v0_testing_20000_42.jpg "Mountain Car")
+
+## Similarly for seed 2
+During training 
+
+|(5k) Failure %|(5k) Avg reward|(20k) Failure %|(20k)Avg reward|
+|---|---|---|---|
+|73.36|-191.9178|63.945|-185.4021|
+
+During testing
+
+|Seed|(5k) Failure %|(5k) Avg reward|(20k) Failure %|(20k)Avg reward|
+|---|---|---|---|---|
+|0|0.0| -162.518|22.0| -158.825|
+|1|0.0| -162.571|23.5| -159.887|
+|2|0.0| -162.512|21.2| -158.827|
+|3|0.0| -162.503|22.7| -159.159|
+|4|0.0| -162.510|24.2| -160.284|
+|5|0.0| -162.431|24.0| -160.091|
+|6|0.0| -162.475|25.4| -160.716|
+|7|0.0| -162.511|22.3| -159.444|
+|8|0.0| -162.463|23.5| -159.820|
+|9|0.0| -162.490|23.5| -159.841|
+
---
+++ RL/q_learning/MountainCar-v0/mountain_car.py
@@ -0,0 +1,75 @@
+import gymnasium as gym
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+
+
+def run(episodes=10000, is_training=True, render=False, np_seed=0, act_seed=42):
+    model_name = "MountainCar-v0"
+    env = gym.make(model_name, render_mode='human' if render else None)
+
+    eps = 1
+    eps_decay = 2/episodes #0.00008
+    disc_fact = 0.9 #0.99
+    lr = 0.9
+    np.random.seed(seed=np_seed)
+    env.action_space.seed(seed=act_seed)
+
+    obs_space = env.observation_space
+    pos_space = np.linspace(obs_space.low[0], obs_space.high[0], 20)
+    vel_space = np.linspace(obs_space.low[1], obs_space.high[1], 20)
+
+    episode_total_rewards = np.zeros(episodes)
+    if is_training:
+        q = np.zeros((len(pos_space), len(vel_space), env.action_space.n)) # 20x20xn
+    else:
+        with open(f'{model_name}.pkl','rb') as f:
+            q = pickle.load(f)
+        # print(q)
+    num_failures = 0
+    for epi in range(episodes):
+        state = env.reset(seed=np.random.randint(0,100))[0]
+        terminated = truncated = False
+        while not terminated and not truncated:
+            state_pos = np.digitize(state[0], pos_space)
+            state_vel = np.digitize(state[1], vel_space)
+            if is_training and eps > np.random.random():
+                action = env.action_space.sample()
+            else:
+                action = np.argmax(q[state_pos][state_vel])
+            next_state, reward, terminated, truncated, _ = env.step(action)
+
+            episode_total_rewards[epi] += reward
+            next_state_pos = np.digitize(next_state[0], pos_space)
+            next_state_vel = np.digitize(next_state[1], vel_space)
+
+            if is_training:
+                target = reward + disc_fact * np.max(q[next_state_pos][next_state_vel])
+                q[state_pos][state_vel][action] += lr*(target - q[state_pos][state_vel][action])
+
+            state = next_state
+
+        if truncated:
+            num_failures += 1
+
+        if is_training:
+            eps = max(0, eps - eps_decay)
+
+    plt.scatter(range(episodes), episode_total_rewards, marker='.')
+    if is_training:
+        plt.title(f'eps decay:{eps_decay} np seed:{np_seed} g:{disc_fact} lr:{lr}')
+        img_name = f'{model_name}_{episodes}_np_{np_seed}_d_{eps_decay}_g_{disc_fact}_lr_{lr}.jpg'
+        f = open(f'{model_name}.pkl','wb')
+        pickle.dump(q, f)
+        f.close()
+    else:
+        img_name = f'{model_name}.jpg'
+    print(num_failures*100/episodes,np.average(episode_total_rewards))
+
+    plt.savefig(img_name)
+
+if __name__ == '__main__':
+    # run(episodes=5000, is_training=True, render=False, np_seed=0)
+    for i in range(10):
+        run(episodes=1000, is_training=False, render=False, np_seed=i)
+    # run(episodes=10, is_training=False, render=True, np_seed=None)
